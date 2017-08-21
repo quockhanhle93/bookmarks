bedf0003949a727af57cb2cfbe644f4feb6803a7
quockhanhle93 <quockhanhle93@gmail.com>
2017-08-21 21:58:26 +0700

    update controllers

--- src/main/java/com/lqkhanh/bookmarks/controller/BookmarkRestController.java
+++ src/main/java/com/lqkhanh/bookmarks/controller/BookmarkRestController.java
@@ -1,13 +1,19 @@
 package com.lqkhanh.bookmarks.controller;
 
+import java.net.URI;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lqkhanh.bookmarks.exception.UserNotFoundException;
 import com.lqkhanh.bookmarks.model.Bookmark;
 import com.lqkhanh.bookmarks.repository.AccountRepository;
 import com.lqkhanh.bookmarks.repository.BookmarkRepository;
@@ -32,11 +38,32 @@ public class BookmarkRestController {
 
 		return this.bookmarkRepository.findByAccountUsername(userId);
 	}
+
+	@PostMapping
+	ResponseEntity<?> add(@PathVariable String userId, @RequestBody Bookmark input) {
+
+		this.validateUser(userId);
+
+		return accountRepository.findByUsername(userId).map(account -> {
+			Bookmark result = bookmarkRepository.save(new Bookmark(account, input.uri, input.description));
+
+			URI location = ServletUriComponentsBuilder
+					.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(result.getId()).toUri();
+			
+				return ResponseEntity.created(location).build();
+		}).orElse(ResponseEntity.noContent().build());
+	}
+	
+	@GetMapping(value = "/{bookmarkId}")
+	Bookmark readBookmark (@PathVariable String userId, @PathVariable Long bookmarkId){
+		this.validateUser(userId);
+		return this.bookmarkRepository.findOne(bookmarkId);
+	}
 	
 
 	private void validateUser(String userId) {
-		// TODO Auto-generated method stub
-
+		this.accountRepository.findByUsername(userId).orElseThrow(() -> new UserNotFoundException(userId));
 	}
 
 }
